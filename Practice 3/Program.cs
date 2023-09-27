c1d91a07c6c570824957686c834ab8fdb008e50c
Nom1o <sindrom.4@yandex.ru>
2023-09-27 17:07:58 +0300

    Complete

--- Practice 3/Program.cs
+++ Practice 3/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
+using System.Text.Json;
+using System.Xml.Serialization;
 
 namespace Practice_3
 {
@@ -43,13 +47,13 @@ namespace Practice_3
       filePath = dictionaryPath + @"\" + fileName;
       fileInfo = new FileInfo(filePath);
     }
-    string dictionaryPath;
-    string fileName;
-    string filePath;
-    DirectoryInfo dirInfo;
-    FileInfo fileInfo;
+    protected string dictionaryPath;
+    protected string fileName;
+    protected string filePath;
+    protected DirectoryInfo dirInfo;
+    protected FileInfo fileInfo;
 
-    public void Update(string fileName)
+    virtual public void Update(string fileName)
     {
       this.fileName = fileName;
       filePath = dictionaryPath + @"\" + fileName;
@@ -71,7 +75,7 @@ namespace Practice_3
     public void InputFile(string text)
     {
       try
-      {        
+      {
         using (StreamWriter writer = fileInfo.AppendText())
         {
           writer.WriteLine(text);
@@ -84,6 +88,21 @@ namespace Practice_3
       }
 
     }
+    public void PrintFile(string fileName)
+    {
+      try
+      {
+        using (StreamReader reader = new StreamReader(dictionaryPath + @"\" + fileName))
+        {
+          string text =  reader.ReadToEnd();
+          Console.WriteLine(text);
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
     public void PrintFile()
     {
       try
@@ -92,7 +111,7 @@ namespace Practice_3
         {
           string text = reader.ReadToEnd();
           Console.WriteLine(text);
-        }        
+        }
       }
       catch (Exception e)
       {
@@ -103,6 +122,168 @@ namespace Practice_3
     {
       fileInfo.Delete();
     }
+    
+
+  }
+
+  class JsonManager : FileManager
+  {
+    public JsonManager() : base()
+    {
+
+    }
+    public JsonManager(string fileName)
+    {
+      this.fileName = fileName + ".json";
+      dictionaryPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\CreatedFiles";
+      dirInfo = new DirectoryInfo(dictionaryPath);
+      filePath = dictionaryPath + @"\" + this.fileName;
+      fileInfo = new FileInfo(filePath);
+    }
+    public override void Update(string fileName)
+    {
+      this.fileName = fileName + ".json";
+      filePath = dictionaryPath + @"\" + this.fileName;
+      fileInfo = new FileInfo(filePath);
+    }
+    public void InputFile(object obj)
+    {
+      try
+      {
+        using (StreamWriter writer = fileInfo.CreateText())
+        {
+          string jsonString = JsonSerializer.Serialize(obj);
+          writer.Write(jsonString);
+
+        }
+        Console.WriteLine("Запись выполнена");
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+
+    }
+  }
+
+  class XmlManager : FileManager
+  {
+    public XmlManager() : base()
+    {
+
+    }
+    public XmlManager(string fileName)
+    {
+      this.fileName = fileName + ".xml";
+      dictionaryPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\CreatedFiles";
+      dirInfo = new DirectoryInfo(dictionaryPath);
+      filePath = dictionaryPath + @"\" + this.fileName;
+      fileInfo = new FileInfo(filePath);
+    }
+    public override void Update(string fileName)
+    {
+      this.fileName = fileName + ".xml";
+      filePath = dictionaryPath + @"\" + this.fileName;
+      fileInfo = new FileInfo(filePath);
+    }
+    public void InputFile<T>(T obj)
+    {
+      try
+      {
+        using (StreamWriter writer = fileInfo.CreateText())
+        {
+          XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+          xmlSerializer.Serialize(writer, obj);
+
+        }
+        Console.WriteLine("Запись выполнена");
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+
+    }
+  }
+  class ZipManager : FileManager
+  {
+    public ZipManager() : base()
+    {
+
+    }
+    string zipName;
+    string zipPath;
+    public void UpdateZip(string zipName)
+    {
+      this.zipName = zipName;
+      zipPath = dictionaryPath + @"\" + zipName + ".zip";
+    }
+    public void CreateZip()
+    {
+      try
+      {
+        using (ZipArchive zipArchive = ZipFile.Open(zipPath, ZipArchiveMode.Create)) { }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
+    public void ToZip()
+    {
+      try
+      {
+        using (var zipArchive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+        {
+          zipArchive.CreateEntryFromFile(filePath, fileName);
+          fileInfo.Delete();
+          Console.WriteLine("В архив добавлена запись!");
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+      }
+    }
+    public void ExtractZip()
+    {
+      List<string> fileNames = new List<string>();
+      using (ZipArchive zipArchive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+      {        
+        foreach (ZipArchiveEntry entry in zipArchive.Entries)
+        {
+          string fileName = entry.FullName;
+          fileNames.Add(fileName);
+        }
+
+      }
+      ZipFile.ExtractToDirectory(zipPath, dictionaryPath);
+      using (ZipArchive zipArchive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+      {
+        while (zipArchive.Entries.Count > 0)
+        {
+          zipArchive.Entries[0].Delete();
+        }
+      }
+      foreach (string item in fileNames)
+      {
+        PrintFile(item);
+      }
+    }
+  }
+  public class SerializableClass
+  {
+    public SerializableClass()
+    {
+
+    }
+    public SerializableClass (string name,string age)
+    {
+      this.name = name;
+      this.age = age;
+    }
+    public string name;
+    public string age;
   }
   class Program
   {
@@ -110,24 +291,51 @@ namespace Practice_3
     {
       Console.WriteLine("1. Вывести информацию в консоль о логических дисках, именах, метке тома, размере и типе файловой системы.");
       Console.WriteLine("2. Работа с файлами");
+      Console.WriteLine("3. Работа с JSON файлами");
+      Console.WriteLine("4. Работа с XML файлами");
+      Console.WriteLine("5. Работа с ZIP файлами");
+      Console.WriteLine("6. Удалить файл");
     }
     static void printFileTasks()
     {
       Console.WriteLine("1. Создать файл");
       Console.WriteLine("2. Записать в файл строку, введённую пользователем");
       Console.WriteLine("3. Прочитать файл в консоль");
-      Console.WriteLine("4. Удалить файл");
-      Console.WriteLine("5. Назад");
+      Console.WriteLine("4. Назад");
+    }
+    static void printJsonTasks()
+    {
+      Console.WriteLine("1. Создать файл");
+      Console.WriteLine("2. Записать в файл объект");
+      Console.WriteLine("3. Прочитать файл в консоль");
+      Console.WriteLine("4. Назад");
+    }
+    static void printXmlTasks()
+    {
+      Console.WriteLine("1. Создать файл");
+      Console.WriteLine("2. Записать в файл объект");
+      Console.WriteLine("3. Прочитать файл в консоль");
+      Console.WriteLine("4. Назад");
+    }
+    static void printZipTasks()
+    {
+      Console.WriteLine("1. Создать архив");
+      Console.WriteLine("2. Добавить файл в архив");
+      Console.WriteLine("3. Разархивировать файл и вывести данные");
+      Console.WriteLine("4. Назад");
     }
     static void Main(string[] args)
-    {    
+    {
       PCInfo pcInfo = new PCInfo();
       FileManager fileManager = new FileManager();
-      printTasks();
+      JsonManager jsonManager = new JsonManager();
+      XmlManager xmlManager = new XmlManager();
+      ZipManager zipManager = new ZipManager();
       string firstTask, secondTask;
       string fileName;
       while (true)
       {
+        printTasks();
         firstTask = Console.ReadLine();
         switch (firstTask)
         {
@@ -175,32 +383,10 @@ namespace Practice_3
                       Console.WriteLine("Введите название файла с расширенем");
                       fileName = Console.ReadLine();
                       fileManager.Update(fileName);
-                      if (fileManager.FileExist())
-                      {
-                        fileManager.PrintFile();
-                      }
-                      else
-                      {
-                        Console.WriteLine("Файл не найден ...");
-                      }
+                      fileManager.PrintFile();
                     }
                     break;
 
-                  case "4":
-                    {
-                      Console.WriteLine("Введите название файла с расширенем");
-                      fileName = Console.ReadLine();
-                      fileManager.Update(fileName);
-                      if (fileManager.FileExist())
-                      {
-                        fileManager.Delete();
-                      }
-                      else
-                      {
-                        Console.WriteLine("Файл не найден ...");
-                      }
-                    }
-                    break;
                   default:
                     fileCaseFlag = false;
                     break;
@@ -209,12 +395,135 @@ namespace Practice_3
               }
             }
             break;
-            
-            
+
+          case "3":
+            {
+              bool jsonCaseFlag = true;
+              while (jsonCaseFlag)
+              {
+                printJsonTasks();
+                secondTask = Console.ReadLine();
+                switch (secondTask)
+                {
+                  case "1":
+                    Console.WriteLine("Введите имя файла без расширения");
+                    fileName = Console.ReadLine();
+                    jsonManager.Update(fileName);
+                    jsonManager.CreateFile();
+                    break;
+                  case "2":
+                    Console.WriteLine("Введите название файла без расширения");
+                    fileName = Console.ReadLine();
+                    jsonManager.Update(fileName);
+                    jsonManager.InputFile(new { name = "Maksim", age = 21 });
+                    break;
+                  case "3":
+                    Console.WriteLine("Введите название файла без расширения");
+                    fileName = Console.ReadLine();
+                    jsonManager.Update(fileName);
+                    jsonManager.PrintFile();
+                    break;
+
+                  default:
+                    jsonCaseFlag = false;
+                    break;
+                }
+              }
+
+            }
+            break;
+
+          case "4":
+            {
+              bool xmlCaseFlag = true;
+              while (xmlCaseFlag)
+              {
+                printXmlTasks();
+                secondTask = Console.ReadLine();
+                switch (secondTask)
+                {
+                  case "1":
+                    Console.WriteLine("Введите имя файла без расширения");
+                    fileName = Console.ReadLine();
+                    xmlManager.Update(fileName);
+                    xmlManager.CreateFile();                    
+                    break;
+                  case "2":
+                    Console.WriteLine("Введите название файла без расширения");
+                    fileName = Console.ReadLine();
+                    xmlManager.Update(fileName);
+                    Console.WriteLine("Введите имя для записи");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Введите возраст для записи");
+                    string age = Console.ReadLine();                    
+                    xmlManager.InputFile(new SerializableClass(name,age));
+                    break;
+                  case "3":
+                    Console.WriteLine("Введите название файла без расширения");
+                    fileName = Console.ReadLine();
+                    xmlManager.Update(fileName);
+                    xmlManager.PrintFile();
+                    break;
+                  default:
+                    xmlCaseFlag = false;
+                    break;
+                }
+              }
+            }
+            break;
+          case "5":
+            {
+              bool zipCaseFlag = true;
+              while (zipCaseFlag)
+              {
+                printZipTasks();
+                secondTask = Console.ReadLine();
+                string zipName;
+                switch (secondTask)
+                {
+                  case "1":
+                    Console.WriteLine("Введите имя архива без расширения");
+                    zipName = Console.ReadLine();
+                    zipManager.UpdateZip(zipName);
+                    zipManager.CreateZip();
+                    break;
+                  case "2":
+                    Console.WriteLine("Введите название архива без расширения");
+                    zipName = Console.ReadLine();
+                    Console.WriteLine("Введите название файла");
+                    fileName = Console.ReadLine();
+                    zipManager.Update(fileName);
+                    zipManager.UpdateZip(zipName);
+                    zipManager.ToZip();
+                    break;
+                  case "3":
+                    Console.WriteLine("Введите название архива без расширения");
+                    zipName = Console.ReadLine();
+                    zipManager.UpdateZip(zipName);
+                    zipManager.ExtractZip();
+                    break;
+                  default:
+                    zipCaseFlag = false;
+                    break;
+                }
+              }
+            }            
+            break;
+          case "6":
+            Console.WriteLine("Введите название файла c расширенем");
+            fileName = Console.ReadLine();
+            fileManager.Update(fileName);
+            if (fileManager.FileExist())
+            {
+              fileManager.Delete();
+            }
+            else
+            {
+              Console.WriteLine("Файл не найден ...");
+            }
+            break;
         }
       }
-
-      Console.WriteLine("Hello World!");
     }
   }
 }
